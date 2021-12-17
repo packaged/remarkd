6b566956b48cf7b0b30dab1a4f565f0f214f2619
Brooke Bryan <brooke@bajb.net>
2021-12-17 14:37:06 +0000

    Add link support

--- src/Objects/References/ReferenceObject.php
+++ src/Objects/References/ReferenceObject.php
@@ -25,7 +25,11 @@ class ReferenceObject extends AbstractRemarkdObject
       $this->_context->meta()->set(self::META_KEY, $references);
       if($ref->content)
       {
-        $this->_context->ruleEngine()->parse($ref->content);
+        $ref->content = $this->_context->ruleEngine()->parse($ref->content);
+      }
+      else if($this->_config['link'])
+      {
+        $ref->content = '<a href="' . $this->_config['link'] . '" target="_blank">' . $this->_config['link'] . '</a>';
       }
 
       return '<a name="remarkd-ref-' . $ref->code . '" href="#remarkd-ref-foot-' . $ref->code . '">[' . $ref->num . ']</a>';
