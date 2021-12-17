961847bdcbe52259d4e4b75b1e840ad05d129e71
Brooke Bryan <brooke@bajb.net>
2021-12-17 15:34:10 +0000

    Update links

--- src/Objects/References/ReferenceListObject.php
+++ src/Objects/References/ReferenceListObject.php
@@ -19,7 +19,7 @@ class ReferenceListObject extends AbstractRemarkdObject
       {
         if($ref instanceof ContentReference)
         {
-          $ol[] = '<li id="remarkd-ref-root-' . $ref->code . '"><a href="#remarkd-ref-foot-' . $ref->code . '">' . $ref->content . '</a></li>';
+          $ol[] = '<li id="rmdref-ft-' . $ref->code . '"><a href="#rmdref-bdy-' . $ref->code . '">^</a> ' . $ref->content . '</li>';
         }
       }
       if(!empty($ol))
