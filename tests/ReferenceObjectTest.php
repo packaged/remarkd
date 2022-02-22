9a8ee5b6ba2654536ca88ad69d2d9cc932e06d6c
Tom Kay <oridan82@gmail.com>
2022-02-22 19:37:30 +0000

    move stranded tests, fix failing ReferenceObjectTest to match published code.

--- tests/ReferenceObjectTest.php
+++ tests/ReferenceObjectTest.php
@@ -17,7 +17,7 @@ Here is some information, we sourced this at {ref, content="http://www.google.co
 MARKDOWN;
 
     $expect = <<<HTML
-<p>Here is some information, we sourced this at <a name="remarkd-ref-1RM" href="#remarkd-ref-foot-1RM">[1]</a>.   For any more info, ask.</p><h1>References</h1><p><ol><li><a name="remarkd-ref-root-1RM" href="#remarkd-ref-foot-1RM">http://www.google.com</a></li></ol></p>
+<p>Here is some information, we sourced this at <sup class="reference"><a name="rmdref-bdy-1RM" href="#rmdref-ft-1RM">[1]</a></sup>.   For any more info, ask.</p><h1>References</h1><p><ol class="reference"><li id="rmdref-ft-1RM"><a href="#rmdref-bdy-1RM" class="reference-tobody">^</a> http://www.google.com</li></ol></p>
 HTML;
 
     self::assertEquals($expect, $remarkd->parse($markdown));
