f6d020952dc01abe998ab0e87217141072bfd560
Brooke Bryan <brooke@bajb.net>
2022-09-05 20:46:56 +0100

    Bold&Italic

--- src/Rules/BoldText.php
+++ src/Rules/BoldText.php
@@ -6,6 +6,6 @@ class BoldText implements RemarkdRule
   public function apply(string $text): string
   {
     $unconstrained = preg_replace('/(\*\*)([^\*]+?)(\*\*)/', '<strong>\2</strong>', $text);
-    return preg_replace('/\*([^*\n]+)\*/', '<strong>\1</strong>', $unconstrained);
+    return preg_replace('/([^\w])\*([^*]+)\*/', '\1<strong>\2</strong>', $unconstrained);
   }
 }
