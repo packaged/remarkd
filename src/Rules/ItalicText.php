706ccae4f5f8be706590b0a9b1029e22f13a37e5
Brooke Bryan <brooke@bajb.net>
2022-09-21 15:07:20 +0100

    Italic

--- src/Rules/ItalicText.php
+++ src/Rules/ItalicText.php
@@ -6,6 +6,6 @@ class ItalicText implements RemarkdRule
   public function apply(string $text): string
   {
     $unconstrained = preg_replace('/(\_\_)([^\_]+?)(\_\_)/', '<em>\2</em>', $text);
-    return preg_replace('/([^\w])\_([^_]+)\_/', '\1<em>\2</em>', $unconstrained);
+    return preg_replace('/(\s|^)\_([^_]+)\_/', '\1<em>\2</em>', $unconstrained);
   }
 }
