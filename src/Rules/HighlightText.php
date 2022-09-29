77467b3e45c0b9af82f22e0e14fdf0e9c4ef35a6
Brooke Bryan <brooke@bajb.net>
2022-09-29 14:49:28 +0100

    Skip characters

--- src/Rules/HighlightText.php
+++ src/Rules/HighlightText.php
@@ -5,6 +5,6 @@ class HighlightText implements RemarkdRule
 {
   public function apply(string $text): string
   {
-    return preg_replace('/[^\#]\#([^\#]+)\#/', '<mark class="highlight">\1</mark>', $text);
+    return preg_replace('/[^\#\&]\#([^\#]+)\#/', '<mark class="highlight">\1</mark>', $text);
   }
 }
