092c718e971aed7148870f80a458eecc491e84e1
Brooke Bryan <brooke@bajb.net>
2022-10-22 23:52:15 +0100

    Support end time and rel=0

--- src/Objects/VideoObject.php
+++ src/Objects/VideoObject.php
@@ -34,17 +34,27 @@ class VideoObject extends AbstractRemarkdObject
     switch($this->_config->get('source', 'youtube'))
     {
       case 'youtube':
-        $content = $this->_completeYoutube($this->_key, $this->_config->get('start', 0));
+        $content = $this->_completeYoutube($this->_key, $this->_config->get('start', 0), $this->_config->get('end', 0));
         break;
     }
 
     return '<div class="video-container"' . $containerAppend . '>' . $content . '</div>';
   }
 
-  protected function _completeYoutube($id, $time = 0)
+  protected function _completeYoutube($id, $start = 0, $end = 0)
   {
+    $opts = [];
+    if($start > 0)
+    {
+      $opts[] = 'start=' . $start;
+    }
+    if($end > 0)
+    {
+      $opts[] = 'end=' . $end;
+    }
+    $opts['rel'] = 0;
     return '<iframe '
-      . 'src="https://www.youtube.com/embed/' . $id . ($time > 0 ? '?start=' . $time : '') . '" '
+      . 'src="https://www.youtube.com/embed/' . $id . (empty($opts) ? '' : '?' . implode(';', $opts)) . '" '
       . 'title="YouTube video player" frameborder="0" '
       . 'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" '
       . 'allowfullscreen></iframe>';
