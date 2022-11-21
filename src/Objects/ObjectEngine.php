0abd58f2f801a2978b936a10e020885731b0768d
Brooke Bryan <brooke@bajb.net>
2022-11-21 16:54:27 +0000

    greedy matching on objects

--- src/Objects/ObjectEngine.php
+++ src/Objects/ObjectEngine.php
@@ -11,7 +11,7 @@ class ObjectEngine
    */
   protected RemarkdContext $_context;
 
-  const objectSelectors = '/{{(%TYPES%)(:([^ }]+))?([^}]*|.*?\"(.|\n)*\".*?)}}/miU';
+  const objectSelectors = '/{{(%TYPES%)(:([^ }]+))?([^}]*|.*?\"(.|\n)*\".*?)}}/mi';
 
   protected $_selector = '';
 
