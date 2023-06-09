bca0be659587294a2c7cc0a89c3f176bb8925426
Kyle Essex <kylegavinessex@hotmail.com>
2023-06-09 14:58:35 +0100

    Run when the document is ready

--- resources/js/tabs.js
+++ resources/js/tabs.js
@@ -1,4 +1,5 @@
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function (e)
+{
   var tab = e.target.getAttribute('data-tab-focus-key');
   if(tab)
   {
@@ -7,19 +8,24 @@ document.addEventListener('click', function (e) {
   }
 });
 
-let url = new URL(window.location.href);
-let tabKey = url.searchParams.get("tab");
-if(tabKey)
+document.addEventListener('DOMContentLoaded', function (e)
 {
-  setActiveTab(tabKey);
-}
+  let url = new URL(window.location.href);
+  let tabKey = url.searchParams.get('tab');
+  if(tabKey)
+  {
+    setActiveTab(tabKey);
+  }
+});
 
 function setActiveTab(tabKey)
 {
   let tabs = document.querySelectorAll('.tabs .tab[data-tab-key=' + tabKey + ']');
-  tabs.forEach(function (tab) {
+  tabs.forEach(function (tab)
+  {
     let tabGroup = tab.parentNode.parentNode;
-    tabGroup.querySelectorAll('.tab-header li a').forEach(function (tabHeader) {
+    tabGroup.querySelectorAll('.tab-header li a').forEach(function (tabHeader)
+    {
       if(tabHeader.getAttribute('data-tab-focus-key') === tabKey)
       {
         tabHeader.classList.add('active');
@@ -29,7 +35,8 @@ function setActiveTab(tabKey)
         tabHeader.classList.remove('active');
       }
     });
-    tab.parentNode.querySelectorAll('.tab').forEach(function (otab) {
+    tab.parentNode.querySelectorAll('.tab').forEach(function (otab)
+    {
       otab.style.display = 'none';
     });
     tab.style.display = 'block';
