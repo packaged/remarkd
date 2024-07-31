53c4f35fe42f8227f9c690940944de159c137709
Kyle Essex <kylegavinessex@hotmail.com>
2024-07-31 16:25:18 +0100

    Fix el

--- resources/js/accordion.js
+++ resources/js/accordion.js
@@ -3,7 +3,7 @@ document.addEventListener('click', function (e)
   const target = e.target;
   if(target.classList.contains('accordion'))
   {
-    this.classList.toggle('active');
+    target.classList.toggle('active');
     const panel = this.nextElementSibling;
     panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + 'px';
   }
